ed9b4e51b05e7397b9ccb1727c2dc35756dd3aa5
Kyosuuke <clementlistpaul59000@gmail.com>
2025-10-07 12:19:39 +0200

    Test

---
+++ Assets/UI/MainMenu.cs
@@ -0,0 +1,11 @@
+using UnityEngine;
+using UnityEngine.UIElements;
+
+namespace UI
+{
+    [RequireComponent(typeof(UIDocument))]
+    public class MainMenu : MonoBehaviour
+    {
+    
+    }
+}
---
+++ Assets/UI/Modals/ResearchPopUp/ResearchPopUp.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+using System.Collections.Generic;
+using UnityEngine.UIElements;
+
+namespace UI.Modals.ResearchPopUp
+{
+    public class ResearchPopUp : UIView
+    {
+        private Button _mailButton;
+
+        public override void Dispose()
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
