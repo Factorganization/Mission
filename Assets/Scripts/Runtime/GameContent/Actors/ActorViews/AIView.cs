6d82a5662041ac976a1ee0dd0d51cf9b2447ce37
Mastrum <hugolesec@live.fr>
2025-10-10 14:54:41 +0200

    Namespace added

--- Assets/Scripts/Runtime/GameContent/Actors/ActorViews/AIView.cs
+++ Assets/Scripts/Runtime/GameContent/Actors/ActorViews/AIView.cs
@@ -1,39 +1,41 @@
 using Runtime.GameContent.Actors.ActorModels;
-using Runtime.GameContent.Actors.ActorViews;
 using UnityEngine;
 
-public class AIView : ActorView
+namespace Runtime.GameContent.Actors.ActorViews
 {
-    [SerializeField] private IAMovementDataSo iaMovementDataSo;
+    public class AIView : ActorView
+    {
+        [SerializeField] private IAMovementDataSo iaMovementDataSo;
     
-    private IAModel _iaModel;
+        private IAModel _iaModel;
     
-    private void Awake()
-    {
-        _iaModel = new IAModel(iaMovementDataSo);
-        _iaModel.transform = transform;
-    }
+        private void Awake()
+        {
+            _iaModel = new IAModel(iaMovementDataSo);
+            _iaModel.transform = transform;
+        }
 
-    private void Start()
-    {
-        IAController.SelectRandomWaypoint(_iaModel);
-    }
+        private void Start()
+        {
+            IAController.SelectRandomWaypoint(_iaModel);
+        }
 
-    private void Update()
-    {
-        if (IAController.RotateToWaypoint(_iaModel))
-            IAController.MoveToWaypoint(_iaModel);
+        private void Update()
+        {
+            if (IAController.RotateToWaypoint(_iaModel))
+                IAController.MoveToWaypoint(_iaModel);
         
-        transform.position = _iaModel.transform.position;
-        transform.rotation = _iaModel.transform.rotation;
+            transform.position = _iaModel.transform.position;
+            transform.rotation = _iaModel.transform.rotation;
         
-        if (_iaModel._currentWaypoint != Vector3.zero)
-            return;
+            if (_iaModel._currentWaypoint != Vector3.zero)
+                return;
         
-        _iaModel._waitTimer += Time.deltaTime;
-        if (!(_iaModel._waitTimer >= iaMovementDataSo.waitDelay)) return;
+            _iaModel._waitTimer += Time.deltaTime;
+            if (!(_iaModel._waitTimer >= iaMovementDataSo.waitDelay)) return;
         
-        IAController.SelectRandomWaypoint(_iaModel);
-        _iaModel._waitTimer = 0;
+            IAController.SelectRandomWaypoint(_iaModel);
+            _iaModel._waitTimer = 0;
+        }
     }
 }
