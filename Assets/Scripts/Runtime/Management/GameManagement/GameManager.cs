e459550b66aa5e4a86f79b079eae3b8d4735d2fd
Brak <clement.strappazzon.marques@outlook.fr>
2025-10-31 19:26:47 +0100

    Setup Gameplay Gym

--- Assets/Scripts/Runtime/Management/GameManagement/GameManager.cs
+++ Assets/Scripts/Runtime/Management/GameManagement/GameManager.cs
@@ -1,9 +1,22 @@
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 namespace Runtime.Management.GameManagement
 {
     public class GameManager : MonoBehaviour
     {
-        //TODO
+        private void Start()
+        {
+            SceneManager.LoadSceneAsync(scene1, LoadSceneMode.Additive);
+            SceneManager.LoadSceneAsync(scene2, LoadSceneMode.Additive);
+        }
+
+        #region fields
+
+        [SerializeField] private string scene1;
+        
+        [SerializeField] private string scene2;
+
+        #endregion
     }
 }
