1c2e2e6b5f7bd3d00f0962899034c47de43de4bc
Brak <clement.strappazzon.marques@outlook.fr>
2025-11-13 12:44:54 +0100

    pre merge

--- Assets/Scripts/Runtime/GameContent/Actors/ActorViews/GrabbableObjectView.cs
+++ Assets/Scripts/Runtime/GameContent/Actors/ActorViews/GrabbableObjectView.cs
@@ -1,6 +1,7 @@
 using Runtime.GameContent.Actors.ActorInterfaces;
 using Runtime.GameContent.Logics.LogicInterfaces;
 using Runtime.GameContent.Logics.LogicModels;
+using Runtime.Management.GameManagement;
 using Shared.Utils.Listing;
 using UnityEngine;
 
@@ -34,6 +35,14 @@ namespace Runtime.GameContent.Actors.ActorViews
 			OriginPos = transform.position;
         }
 
+        public void Update()
+        {
+	        foreach (var e in LevelGenerator.Generator.ElementHolders)
+	        {
+		        
+	        }
+        }
+
 		public bool Action()
 		{
 			return false;
@@ -41,18 +50,56 @@ namespace Runtime.GameContent.Actors.ActorViews
 
 		public void CheckOtherElement(ElementFlag elementFlag)
 		{
+			
+		}
 
+		protected void SetParticle(ElementFlag elementFlag)
+		{
+			if ((elementFlag & ElementFlag.CanBeWet) != 0)
+				vfxReferences.waterParticles.Play();
+			else 
+				vfxReferences.waterParticles.Stop();
+			
+			if ((elementFlag & ElementFlag.CanBurn) != 0)
+				vfxReferences.fireParticles.Play();
+			else 
+				vfxReferences.fireParticles.Stop();
+			
+			if ((elementFlag & ElementFlag.CanConduct) != 0)
+				vfxReferences.electricParticles.Play();
+			else 
+				vfxReferences.electricParticles.Stop();
+			
+			if ((elementFlag & ElementFlag.CanExplode) != 0)
+				vfxReferences.explosionParticles.Play();
+			else 
+				vfxReferences.explosionParticles.Stop();
+			
 		}
 
 		#endregion
 
 		#region fields
 
+		[SerializeField] private VFXReferences vfxReferences;
+		
 		[SerializeField] private ElementFlag element;
-
+		
 		private Rigidbody _rb;
 
         private MeshRenderer _meshRenderer;
+
+        [System.Serializable]
+        private class VFXReferences
+        {
+	        [SerializeField] internal ParticleSystem fireParticles;
+	        
+	        [SerializeField] internal ParticleSystem waterParticles;
+	        
+	        [SerializeField] internal ParticleSystem electricParticles;
+	        
+	        [SerializeField] internal ParticleSystem explosionParticles;
+        }
 
         #endregion
     }
---
+++ Assets/Scripts/Runtime/Management/GameManagement/ElementManager.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+
+namespace Runtime.Management.GameManagement
+{
+    public class ElementManager : MonoBehaviour
+    {
+        #region properties
+
+        
+
+        #endregion
+        
+        #region methodes
+
+        
+
+        #endregion
+    }
+}
