e08cf5fd99ccf62582a53e035f4fec82b2b1748f
Kyosuuke <92298804+Kyosuuke@users.noreply.github.com>
2025-12-28 23:34:23 +0100

    [ADD] Ajout rapide d'animations pour la custom

--- Assets/Scripts/Runtime/Services/Game/GameContent/UI/Customization/CustomizeButton.cs
+++ Assets/Scripts/Runtime/Services/Game/GameContent/UI/Customization/CustomizeButton.cs
@@ -1,101 +1,101 @@
 using UnityEngine.Events;
 using UnityEngine.UI;
 
-namespace Runtime.Services.Game.GameContent.UI.Customization;
-
-[Serializable]
-public class ChangeSkin : UnityEvent<CustomizeButton> { }
-
-public class CustomizeButton : MonoBehaviour
+namespace Runtime.Services.Game.GameContent.UI.Customization
 {
-    #region Functions
+    [Serializable]
+    public class ChangeSkin : UnityEvent<CustomizeButton> { }
 
-    private void Start()
+    public class CustomizeButton : MonoBehaviour
     {
-        Initialize();
-    }
+        #region Functions
 
-    private void Initialize()
-    {
-            
-        if (_image != null && _customizeItem != null)
-            _image.sprite = _customizeItem.ItemIcon;
+        private void Start()
+        {
+            Initialize();
+        }
 
-        if (_lockImage != null)
-            _lockImage.gameObject.SetActive(_locked);
-    }
-        
-    public void OnSelected() 
-    {
-        if (_locked) return;
-        OnChangeSkin.Invoke(this);
-    }
-        
-    public void SetDataPrefab(GameObject prefab, Sprite icon, bool locked, int index)
-    {
-        _customizeItem = null;
-        SelectedPrefab = prefab;
-        ItemIndex = index;
-        _locked = locked;
+        private void Initialize()
+        {
 
-        if (_image != null)
-            _image.sprite = icon;
+            if (_image != null && _customizeItem != null)
+                _image.sprite = _customizeItem.ItemIcon;
 
-        if (_lockImage != null)
-            _lockImage.gameObject.SetActive(_locked);
-    }
-        
-    public void SetDataMesh(Mesh mesh, Sprite icon, bool locked, int index)
-    {
-        _customizeItem = null;
-        SelectedPrefab = null;
-        ItemIndex = index;
-        _locked = locked;
-        SelectedMaterial = null;
-        SelectedMesh = mesh;
+            if (_lockImage != null)
+                _lockImage.gameObject.SetActive(_locked);
+        }
 
-        if (_image != null)
-            _image.sprite = icon;
+        public void OnSelected()
+        {
+            if (_locked) return;
+            OnChangeSkin.Invoke(this);
+        }
 
-        if (_lockImage != null)
-            _lockImage.gameObject.SetActive(_locked);
-    }
-        
-    public void SetDataMat(Material mat, Sprite icon, bool locked, int index)
-    {
-        _customizeItem = null;
-        SelectedPrefab = null;
-        ItemIndex = index;
-        _locked = locked;
-        SelectedMaterial = mat;
-        SelectedMesh = null;
+        public void SetDataPrefab(GameObject prefab, Sprite icon, bool locked, int index)
+        {
+            _customizeItem = null;
+            SelectedPrefab = prefab;
+            ItemIndex = index;
+            _locked = locked;
 
-        if (_image != null)
-            _image.sprite = icon;
-            
-        if (_lockImage != null) 
-            _lockImage.gameObject.SetActive(_locked);
-    }
-        
-    public void ResetForPool() 
-    {
-        OnChangeSkin.RemoveAllListeners();
-        _customizeItem = null;
-        _locked = false;
-        if (_image != null) _image.sprite = null;
-        if (_lockImage != null) _lockImage.gameObject.SetActive(false);
-    }
+            if (_image != null)
+                _image.sprite = icon;
 
-    #endregion
-        
+            if (_lockImage != null)
+                _lockImage.gameObject.SetActive(_locked);
+        }
+
+        public void SetDataMesh(Mesh mesh, Sprite icon, bool locked, int index)
+        {
+            _customizeItem = null;
+            SelectedPrefab = null;
+            ItemIndex = index;
+            _locked = locked;
+            SelectedMaterial = null;
+            SelectedMesh = mesh;
+
+            if (_image != null)
+                _image.sprite = icon;
+
+            if (_lockImage != null)
+                _lockImage.gameObject.SetActive(_locked);
+        }
+
+        public void SetDataMat(Material mat, Sprite icon, bool locked, int index)
+        {
+            _customizeItem = null;
+            SelectedPrefab = null;
+            ItemIndex = index;
+            _locked = locked;
+            SelectedMaterial = mat;
+            SelectedMesh = null;
+
+            if (_image != null)
+                _image.sprite = icon;
+
+            if (_lockImage != null)
+                _lockImage.gameObject.SetActive(_locked);
+        }
+
+        public void ResetForPool()
+        {
+            OnChangeSkin.RemoveAllListeners();
+            _customizeItem = null;
+            _locked = false;
+            if (_image != null) _image.sprite = null;
+            if (_lockImage != null) _lockImage.gameObject.SetActive(false);
+        }
+
+        #endregion
+
         #region Fields
 
         [SerializeField] private Image _image;
         [SerializeField] private Image _lockImage;
-        
+
         [SerializeField] private CustomizeItem _customizeItem;
         [SerializeField] private bool _locked;
-        
+
         public ChangeSkin OnChangeSkin = new ChangeSkin();
 
         public GameObject SelectedPrefab { get; private set; }
@@ -104,4 +104,5 @@ public class CustomizeButton : MonoBehaviour
         public int ItemIndex { get; private set; }
 
         #endregion
+    }
 }
