713e3bbd959aeb5453c1eeba2cf94e8ac0abe5c1
Kyosuuke <clementlistpaul59000@gmail.com>
2026-01-23 14:45:10 +0100

    [ADD] Anim menu

--- Assets/Scripts/Runtime/Services/Game/GameContent/UI/UIParent.cs
+++ Assets/Scripts/Runtime/Services/Game/GameContent/UI/UIParent.cs
@@ -3,6 +3,12 @@ namespace Runtime.Services.Game.GameContent.UI
     public class UIParent : MonoBehaviour
     {
         #region Functions
+
+        private void Start()
+        {
+            _isOpen = false;
+        }
+
         public virtual void Show()
         {
             gameObject.SetActive(true);
@@ -11,14 +17,13 @@ namespace Runtime.Services.Game.GameContent.UI
         public virtual void Hide()
         {
             gameObject.SetActive(false);
+            _isOpen = false;
         }
         #endregion
 
         #region Fields
 
-        [SerializeField] private bool _isOpen;
-        
-        public bool isOpen => _isOpen;
+        [SerializeField] protected bool _isOpen;
 
         #endregion
     }
