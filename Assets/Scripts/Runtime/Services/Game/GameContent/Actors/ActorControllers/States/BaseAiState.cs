b26ce6687c195a6308db4840f557cdc507f04e75
Mastrum <hugolesec@live.fr>
2026-01-23 14:51:29 +0100

    Little changes on AI States

--- Assets/Scripts/Runtime/Services/Game/GameContent/Actors/ActorControllers/States/BaseAiState.cs
+++ Assets/Scripts/Runtime/Services/Game/GameContent/Actors/ActorControllers/States/BaseAiState.cs
@@ -27,7 +27,6 @@ namespace Runtime.Services.Game.GameContent.Actors.ActorControllers.States
 
         public override sbyte OnUpdate()
         {
-            aiModel._animatorRef.SetFloat("speed", aiModel._agentRef.velocity.magnitude);
             if (AIController.DetectPlayer(aiModel))
             {
                 stateMachine.SwitchState("suspicious");
